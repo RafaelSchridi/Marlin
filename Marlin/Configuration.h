877dcbd59a51c9283646d4609758669c917b69c8
Rafael Schridi <r.schridi@gmail.com>
2020-11-26 15:28:19 +0100

    reduce print size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1131,8 +1131,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 240
-#define Y_BED_SIZE 240
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
